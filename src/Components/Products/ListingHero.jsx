5fa282c9ff77c28ad21d71ae3b4974a8de14c980
AnujPancholiGithub <helpeventanuj@gmail.com>
2023-03-14 13:57:22 +0530

    second commit

---
+++ src/Components/Products/ListingHero.jsx
@@ -0,0 +1,14 @@
+import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
+
+
+function ListingHero() {
+    return (<Box >
+        <Stack textAlign="left">
+            <Heading>Wifi-Speakers</Heading>
+            <Text>What do you get when you combine great home furnishing knowledge with sound expertise? The answer is SYMFONISK, an all-new sound range created by Sonos and IKEA. Release the sweet sound of your home with a table lamp and bookshelf speakers that have high-quality WiFi sound.</Text>
+            <Link>Learn even more about SYMFONISK WiFi speakers</Link>
+        </Stack>
+    </Box >);
+}
+
+export default ListingHero;
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,1 +1,8 @@
-k
+import { Box } from "@chakra-ui/react";
+import ListingHero from "./ListingHero";
+
+function Products() {
+    return (<Box> <ListingHero /></Box>)
+}
+
+export default Products;
