9a7b0d8cae3c329c971e443ffb0555459f6211f9
pranshushuklagithub <pranshushuklaoffice@gmail.com>
2023-03-16 14:54:00 +0530

    emptycart compo created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import AddToCart from './Components/AddToCart';
+import EmptyCart from './Components/EmptyCart';
 
 function App() {
   return (
     <div className="App">
       
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <EmptyCart/>
       
     </div>
     
---
+++ src/Components/EmptyCart.jsx
@@ -0,0 +1,70 @@
+
+
+
+function EmptyCart() {
+    
+     
+    return (
+        <div className="parent-div-cart" style={{
+            width : "70%",
+            height : "100%",
+            margin : "auto",
+            display : "flex",
+            padding : "7%",
+        }}>
+            
+            <div  style={{
+                width : "100%",
+                textAlign:"left",
+                paddingRight : "10%",
+                paddingTop : "10%",
+                paddingBottom : "10%",
+                justifyContent : "left",
+                boxShadow : "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+            }}> 
+            <h1>Track & manage my order</h1>
+                <div style={{
+                    width:"100%",
+                    textAlign:"left",
+                    boxShadow : "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                    paddingRight : "5%",
+                    paddingTop : "5%",
+                    paddingBottom : "5%",
+                    justifyContent : "left"
+                }} ><p>The list of past purchases is not currently available. If you have an order number for your purchase, you can still use it to view and manage your order.</p>
+                </div>
+            </div>
+            
+            <div style={{
+                width:"100%",
+                textAlign : "left",
+                padding : "5%",
+            }}>
+            
+                
+                <input type="text" placeholder="Order number" style={{
+                    width:"98%",
+                }} />
+                <p>9-10 digits with no spaces.</p>
+                
+                <input type="text" placeholder="Email address or phone number" style={{
+                    width:"98%",
+                }}/>
+                <p>The email or phone number used when placing the order.</p>
+                
+                
+                <button style={{
+                    color : "white",
+                    borderRadius : "20%",
+                    width : "50%",
+                    backgroundColor : "black",
+                    padding : "3%"
+                }}>Look up order</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default EmptyCart;
+
