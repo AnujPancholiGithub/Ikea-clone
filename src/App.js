3c58969e97fcd211b27558928013eac90aa37910
s17200 <sonvanesaurabh7@gmail.com>
2023-03-18 20:23:37 +0530

    signup login completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Login from './components/login/Login';
+import AllRoutes from './components/Routes/AllRoutes';
 import Signup from './components/signup/Signup';
 
 
 function App() {
   return (
-     <Signup/>
+     <AllRoutes/>
     
   );
 }
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,10 +3,11 @@ import {
     Box, Flex, Heading, Input, Text, Button, FormControl, FormLabel,
     FormErrorMessage,
     FormHelperText,
-    Image, Link, Checkbox, InputRightElement, InputGroup, Tooltip
+    Image, Checkbox, InputRightElement, InputGroup, Tooltip
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { ArrowBackIcon, InfoIcon, ViewIcon, WarningIcon } from '@chakra-ui/icons'
+import { Link } from 'react-router-dom';
 
 
 export default function Login() {
