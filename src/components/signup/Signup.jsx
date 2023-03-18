5b6645505b8be723f7328ebcff5be206fbe84a85
s17200 <sonvanesaurabh7@gmail.com>
2023-03-18 10:52:27 +0530

    up

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {
+    Box, Flex, Heading, Input, Text, Button, FormControl, FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Image, Link, Checkbox, InputRightElement, InputGroup, Tooltip
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { ArrowBackIcon, InfoIcon, ViewIcon, WarningIcon } from '@chakra-ui/icons'
 
 export default function Signup() {
   return (
