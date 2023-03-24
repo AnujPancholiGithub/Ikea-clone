d8a024db896586dc92ed3b7de1b6f39a10ff0878
AnujPancholiGithub <helpeventanuj@gmail.com>
2023-03-24 12:31:02 +0530

    re-construct first commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { Provider } from 'react-redux';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
